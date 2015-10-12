d9b6417e08ad939a021339d0e04e41e0e0a9fbf8
Jacob <jacob@mpulsedesign.com>
2015-10-12 00:15:13 -0500

    added a state machine for counting tickets - updated variables to be more clear - more inline comments

--- functions_test.c
+++ functions_test.c
@@ -1,34 +1,36 @@
 // Ticket dispenser test script for Deltronic Labs DL-1275 ticket dispenser & Arduino Uno
 
-int tsignal = 2; // opto-sensor pin# (blue wire on DL-1275)
-int tmotor = 7; // motor control pin# (white wire on DL-1275)
+int tsignal = 12; // opto-sensor pin# (blue wire on DL-1275)
+int tmotor = 11; // motor control pin# (white wire on DL-1275)
 int tcount = 0; // counter for # of tickets to dispense
 int tsignal_status = 0; // listner for opto-sensor LOW / HIGH status
 
 void setup() {
 
-	// initialize serial
-	Serial.begin(9600);
+  // initialize serial
+  Serial.begin(9600);
 
-	// setup the pins
-	pinMode(tsignal, INPUT_PULLUP);
-	pinMode(tmotor, OUTPUT);
+  // setup the pins
+  pinMode(tsignal, INPUT_PULLUP);
+  pinMode(tmotor, OUTPUT);
 }
 
 void loop() {
 
-	// read the LOW / HIGH status of tsignal (opto-sensor)
-	int tsignal_status = digitalRead(tsignal);
+  // read the LOW / HIGH status of tsignal (opto-sensor)
+  int tsignal_status = digitalRead(tsignal);
 
-	// output the LOW / HIGH status of opto-sensor
-	Serial.println(tsignal_status);
+  // output the LOW / HIGH status of opto-sensor
+  //Serial.println(tsignal_status);
 
-	// turn motor on when opto-sensor is broken - default off (LOW)
-	if (tsignal_status == LOW)
-	{
-		digitalWrite(tmotor, LOW);
-	}
-	else {
-		digitalWrite(tmotor, HIGH);
-	}
+  // turn motor on when opto-sensor is broken - default off (LOW)
+  if (tsignal_status == LOW)
+  {
+    Serial.println("Signal status is LOW");
+  }
+  else if (tsignal_status == HIGH) {
+    Serial.println("Signal status is HIGH");
+  } else {
+    Serial.println("Nothing detected...");
+  }
 }
